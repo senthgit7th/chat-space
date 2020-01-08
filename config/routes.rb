ad83c0e01e9956aa89bce9df7719ee4511593ac2
senthgit7th <ryo0514s.0707@outlook.jp>
2020-01-08 14:06:00 +0900

    グループ機能の実装

--- config/routes.rb
+++ config/routes.rb
@@ -1,8 +1,6 @@
 Rails.application.routes.draw do
   devise_for :users
-  get 'messages/index'
-
-  root "messages#index"
-  resources :users, only: [:edit, :update]
-  # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
+  root 'groups#index'
+  resources :users, only: [:index, :edit, :update]
+  resources :groups, only: [:index, :new, :create, :edit, :update]
 end
