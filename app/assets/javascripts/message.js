aebddc7c853c8bc2250f2411948e3f3b5b53ac13
senthgit7th <ryo0514s.0707@outlook.jp>
2020-01-16 14:53:52 +0900

    自動更新実装#01

--- app/assets/javascripts/message.js
+++ app/assets/javascripts/message.js
@@ -1,4 +1,5 @@
 $(function(){ 
+  last_message_id = $('.message:last').data("message-id");
   function buildHTML(message){
    if ( message.image ) {
      var html =
@@ -44,29 +45,56 @@ $(function(){
    };
  }
  
-$('#new_message').on('submit', function(e){
- e.preventDefault();
- var formData = new FormData(this);
- var url = $(this).attr('action')
- $.ajax({
-   url: url,
-   type: "POST",
-   data: formData,
-   dataType: 'json',
-   processData: false,
-   contentType: false
- })
-  .always(function(){
-    $(".submit-btn").prop("disabled", false);
+  $('#new_message').on('submit', function(e){
+  e.preventDefault();
+  var formData = new FormData(this);
+  var url = $(this).attr('action')
+  $.ajax({
+    url: url,
+    type: "POST",
+    data: formData,
+    dataType: 'json',
+    processData: false,
+    contentType: false
   })
-  .done(function(data){
-    var html = buildHTML(data);
-    $('.messages').append(html);
-    $('form')[0].reset();
-    $('.messages').animate({ scrollTop: $('.messages')[0].scrollHeight});
+    .always(function(){
+      $(".submit-btn").prop("disabled", false);
+    })
+    .done(function(data){
+      var html = buildHTML(data);
+      $('.messages').append(html);
+      $('form')[0].reset();
+      $('.messages').animate({ scrollTop: $('.messages')[0].scrollHeight});
+    })
+    .fail(function() {
+      alert('メッセージ送信に失敗しました');
+    });
   })
-  .fail(function() {
-    alert('メッセージ送信に失敗しました');
-  });
-})
+  var reloadMessages = function() {
+    var last_message_id = $(".main-chat__main-view:last").data("message-id");
+    $.ajax({
+      url: "api/messages",
+      type: 'get',
+      dataType: 'json',
+      data: {id: last_message_id}
+    })
+    .done(function(messages) {
+      if (messages.length !== 0) {
+        var insertHTML = '';
+        $.each(messages, function(i, message) {
+          insertHTML += buildHTML(message)
+        });
+        $('.messages').append(insertHTML);
+        $('.messages').animate({ scrollTop: $('.messages')[0].scrollHeight});
+        $("#new_message")[0].reset();
+        $(".form__submit").prop("disabled", false);
+      }
+    })
+    .fail(function() {
+      alert('自動更新に失敗しました');
+    })
+  };
+  if (document.location.href.match(/\/groups\/\d+\/messages/)) {
+    setInterval(reloadMessages, 7000);
+  }
 });
