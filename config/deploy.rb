1c4e9cd0e31b1d5d92c588c275a643bcefd4ebcf
senthgit7th <ryo0514s.0707@outlook.jp>
2020-01-18 14:37:21 +0900

    S3/security update

--- config/deploy.rb
+++ config/deploy.rb
@@ -2,6 +2,7 @@ lock "~> 3.11.2"
 
 set :application, "chat-space"
 set :repo_url, "git@github.com:senthgit7th/chat-space.git"
+set :linked_files, %w{ config/secrets.yml }
 
 set :linked_dirs, fetch(:linked_dirs, []).push('log', 'tmp/pids', 'tmp/cache', 'tmp/sockets', 'vendor/bundle', 'public/system', 'public/uploads')
 
@@ -20,4 +21,16 @@ namespace :deploy do
   task :restart do
     invoke 'unicorn:restart'
   end
+
+  desc 'upload secrets.yml'
+  task :upload do
+    on roles(:app) do |host|
+      if test "[ ! -d #{shared_path}/config ]"
+        execute "mkdir -p #{shared_path}/config"
+      end
+      upload!('config/secrets.yml', "#{shared_path}/config/secrets.yml")
+    end
+  end
+  before :starting, 'deploy:upload'
+  after :finishing, 'deploy:cleanup'
 end
